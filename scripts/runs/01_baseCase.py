bc6de640c3fa28a5c49d237bfa18eb1eb7338bd9
Thibaut Lunet <thibaut.lunet@tuhh.de>
2026-08-07 15:05:59 +0200

    TL: moved battery experiments into batpint

--- scripts/runs/01_baseCase.py
+++ scripts/runs/01_baseCase.py
@@ -4,6 +4,7 @@ from time import time
 from pprint import pp as pprint
 from batpint import plt
 import batpint.parameter.javid as bpar
+from batpint.problems.battery import batteryProblem
 
 # Script parameters
 half_cell = True
@@ -32,43 +33,7 @@ var_pts = {
 parameter_values=pybamm.ParameterValues(
     bpar.Li_half.PARAMS if half_cell else bpar.Li_full.PARAMS)
 
-if exp == "GITT":
-    # define GITT experiment: short pulse followed by long rest period
-    pulse_duration = "10 minutes"  # duration of current pulse
-    rest_duration = "2 hours"      # relaxation time
-    current_rate = "0.05C"         # current during pulse
-    voltage_min = 2.5              # V - discharge cutoff
-
-    # WARNING : this parameter is not used !
-    voltage_max = 4.2              # V - charge cutoff
-
-    # Create GITT discharge experiment
-    # Tuple groups discharge+rest as ONE cycle
-    experiment = pybamm.Experiment(
-        [
-            (
-                f"Discharge at {current_rate} for {pulse_duration}",
-                f"Rest for {rest_duration}"
-            )
-        ] * nCycles,
-        termination=f"{voltage_min} V"
-    )
-elif exp == "CCCV":
-    # define CCVV experiment: many cycles of discharge / charge
-    experiment = pybamm.Experiment(
-        [
-            (
-            #"Rest for 5 minutes",
-            "Discharge at 2C until 3.5 V",
-            "Rest for 5 minutes",
-            "Charge at 0.1C until 4.2 V",
-            "Rest for 10 minutes"
-            )
-        ]
-        * nCycles
-        )
-else:
-    raise NotImplementedError(f"{exp=}")
+experiment = batteryProblem(nCycles, expType=exp)
 
 # Setup solver
 if sType == "CASADI":
