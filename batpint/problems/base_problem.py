98b3141c97d6cd15b7ee5e3e42766f0eb47e0f6a
Yessim Aigerim <yessima@hsu-hh.de>
2026-08-25 10:21:52 +0200

    created generic "problem" class for ivp

---
+++ batpint/problems/base_problem.py
@@ -0,0 +1,51 @@
+class Problem:
+    """
+    The problem (IVP) is defined by
+        du/dt = f(t, u), u(t0) = u0.
+    Optional functions may be supplied for the Jacobian of the
+    right-hand side and for event detection.
+    Additional keyword arguments passed at construction are stored
+    as fixed problem parameters.
+    
+    Parameters
+    ----------
+    t0 : float
+        Initial time.
+    u0 : scalar or array-like
+        Initial state.
+    rhs : callable
+        Right-hand side f(t, u, ...).
+    jacobian : callable, optional
+        Jacobian of the right-hand side.
+    event : callable, optional
+        Event function g(t, u, ...).
+    **params
+        Fixed parameters of the mathematical problem.
+    """
+
+    def __init__(self, t0, u0, rhs, jacobian=None, event=None, **params):
+        # Original initial condition of the IVP
+        self.t_start = t0
+        self.u_start = u0
+        
+        self.rhs = rhs
+        self.jacobian = jacobian
+        self.event = event
+
+        self.params = params
+
+    def __call__(self, t, u, **kwargs):
+        """Evaluate the right-hand side f(t, u)."""
+        return self.rhs(t, u, **self.params, **kwargs)
+
+    def jacobian_value(self, t, u, **kwargs):
+        """Evaluate the Jacobian of the right-hand side."""
+        if self.jacobian is None: 
+            raise ValueError("No Jacobian function defined for this problem.")
+        return self.jacobian(t, u, **self.params, **kwargs)
+
+    def event_value(self, t, u, **kwargs):
+        """Evaluate the event function g(t, u)."""
+        if self.event is None: 
+            raise ValueError("No event function defined for this problem.")
+        return self.event(t, u, **self.params, **kwargs)
