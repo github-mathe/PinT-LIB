2c0d1b05e697b62f5689a7ef4d2b8277d074896a
Yessim Aigerim <yessima@hsu-hh.de>
2026-09-02 18:44:07 +0200

    changed order of positional arguments, modified the constructor of the class

--- batpint/time_integration/base_method.py
+++ batpint/time_integration/base_method.py
@@ -1,8 +1,23 @@
+import inspect
 class IntegrationMethod(object):
     """
     Base class for integration methods.
     """
-    def step(self, rhs, t, u, h, *args, **kwargs):
+    def __init__(self):
+        self.step_params = self._get_params()
+
+    def _get_params(self):
+        """
+        Get the parameters of the integration method.
+        Returns:
+            tuple: A tuple containing the parameters of the integration method.
+        """
+        names = tuple(inspect.signature(self.step).parameters)
+        if names[:5] != ("t", "u", "h", "rhs", "jacobian"):
+            raise ValueError("Integration method step function must start with arguments (t, u, h, rhs, jacobian).")
+        return names[5:]
+
+    def step(self, t, u, h, rhs, jacobian=None, *args, **kwargs):
         """
         Perform a single integration step.
         """
