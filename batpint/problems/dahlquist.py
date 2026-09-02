5fe3f514e93b2c847a402ba4f379f11847e50eb0
Yessim Aigerim <yessima@hsu-hh.de>
2026-09-02 19:18:51 +0200

    added solver for  a given number of cycles

--- batpint/problems/dahlquist.py
+++ batpint/problems/dahlquist.py
@@ -1,7 +1,8 @@
 import numpy as np
 from batpint.problems.base_problem import Problem
 from batpint.time_integration.base_method import IntegrationMethod
-
+from batpint.timestepping.timestepper import TimeStepper
+from batpint.parareal.timestepper_propagator import TimeStepperPropagator
 
 class Dahlquist(Problem):
     """
@@ -79,3 +80,47 @@ class DahlquistExact(IntegrationMethod):
         C = np.exp(-lam_cycle * t) * (u + (alpha * np.cos(alpha * t) + lam_cycle * np.sin(alpha * t)) / denominator)
 
         return (C * np.exp(lam_cycle * t_next) - (alpha * np.cos(alpha * t_next) + lam_cycle * np.sin(alpha * t_next)) / denominator)
+
+def solve_dahlquist_cycles(problem, method, dt, num_cycles, make_state, terminate_cycle = None, direction=1, save_history=True):
+    """
+    solve_dahlquist solves the Dahlquist problem using the specified time stepper method.
+    Args:
+        dt (float): time step size
+        num_cycles (int): number of cycles to simulate
+        method (TimeStepper): time stepping method to use
+        save_history (bool, optional): whether to save the history of the time stepping. Defaults to True.
+
+    Returns:
+        tuple: (t_all, u_all, t_ev, u_ev) where t_all and u_all are the time and solution arrays for the entire simulation, 
+        and t_ev and u_ev are the time and solution arrays at the event points.
+    """
+    
+    t_ev = np.zeros(num_cycles+1)
+    t_ev[0] = problem.t_start
+    u_ev = np.zeros(num_cycles+1, dtype=complex)
+    u_ev[0] = problem.u_start
+    states = []
+    t_all = [problem.t_start]
+    u_all = [problem.u_start]
+
+    timestepper = TimeStepper(problem=problem, method=method, dt=dt, save_history=save_history)
+    propagator = TimeStepperPropagator(timestepper=timestepper, direction=1, terminate_cycle = terminate_cycle)
+    
+    for ev in range(num_cycles):
+        
+        current_state = make_state(t_ev[ev], u_ev[ev], ev)
+        states.append(current_state)
+        t_new, u_new = propagator.propagate(state=current_state)
+
+        t_ev[ev+1] = t_new
+        u_ev[ev+1] = u_new
+        
+        # extract solution for the current cycle
+        t_local = np.asarray(propagator.history['t'])
+        u_local = np.asarray(propagator.history['u'])
+        
+        # save the solution for the current cycle
+        t_all.extend(t_local[1:])
+        u_all.extend(u_local[1:])
+        
+    return np.asarray(t_all), np.asarray(u_all), np.asarray(t_ev), np.asarray(u_ev)
