8e21dc2a935b07d6c1d88ac125172cd6c574c576
Yessim Aigerim <yessima@hsu-hh.de>
2026-09-02 19:00:23 +0200

    convert parareal function to class

--- batpint/parareal/parareal.py
+++ batpint/parareal/parareal.py
@@ -1,6 +1,82 @@
 import numpy as np
+import copy
 
-def PararealModified(F, G, t0, u0, N, K):
+class PararealModified:
+    """
+    Generic Parareal solver operating on Propagator objects.
+
+    Parameters
+    ----------
+    fine : Propagator
+        Fine propagator F.
+    coarse : Propagator
+        Coarse propagator G.
+    make state : callable
+        Function
+            make_state(t, u, n)
+        returning the propagation state for cycle n.
+    """
+
+    def __init__(self, fine, coarse, make_state):
+        self.fine = fine
+        self.coarse = coarse
+        self.make_state = make_state
+
+    def solve(self, t0, u0, K, N):
+        """
+        Perform Parareal iterations.
+
+        Parameters
+        ----------
+        K : int
+            Number of Parareal iterations.
+        N : int
+            Number of time windows (coarse time grid).
+
+        Returns
+        -------
+        TT : np.ndarray
+            Array of shape (K+1, N+1) containing the time values for each iteration and window.
+        U : np.empty
+            Array of shape (K+1, N+1) containing the solution values for each iteration and window.
+        """
+        print("Initializing Parareal solver...")
+        TT          =   np.zeros((K+1, N+1), dtype = float)
+        TT[:,0]     =   t0
+        
+        U           =   np.empty((K+1, N+1), dtype = object)  
+
+        def parareal_state(k, n):
+            return self.make_state(TT[k, n], U[k, n], n)
+
+        for k in range(K+1):
+            U[k, 0]     =   copy.deepcopy(u0)
+
+        print("Initialization completed.")
+        
+        # First coarse propagation
+        print("Starting first coarse propagation...")
+        for n in range(N):
+            TPG, UPG    =   self.coarse.propagate(parareal_state(0, n))
+            TT[0,n+1]           =   TPG
+            U[0, n+1]           =   copy.deepcopy(UPG)
+
+        print("First coarse propagation completed.")
+        print("Starting Parareal iterations...")
+        # Parareal iterations
+        for k in range(K):  
+            for n in range(N):
+        
+                TPF, UPF     =    self.fine.propagate(parareal_state(k, n)) 
+                TPG, UPG     =    self.coarse.propagate(parareal_state(k, n))
+                
+                TPG_s, UPG_s =    self.coarse.propagate(parareal_state(k+1, n))
+                TT[k+1, n+1] =    TPF - TPG + TPG_s 
+                U[k+1, n+1]  =    UPF - UPG + UPG_s   
+        print("Parareal iterations completed.")
+        return TT, U
+    
+def PararealOld(F, G, t0, u0, N, K):
     
     TT          =   np.zeros((K+1, N+1), dtype = float)
     TT[:,0]     =   t0
