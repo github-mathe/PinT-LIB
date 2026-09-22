c7ef36a6328fb11cc831bbde41b50dab642f846a
Yessim Aigerim <yessima@hsu-hh.de>
2026-09-22 16:10:58 +0200

    state comparison in dfn

--- scripts/runs/03_dfnParareal.py
+++ scripts/runs/03_dfnParareal.py
@@ -35,15 +35,51 @@ sim = pybamm.Simulation(
     experiment=experiment,
     parameter_values=parameter_values,
     solver=solver,
-    var_pts=var_pts)
+    var_pts=var_pts,)
+
 sim.build_for_experiment()
 
-sol1=sim.solve()
+# stepwise implementation
+solutions = {}
+num_cycles = 10
+for cycle in range(num_cycles):
+    if cycle==0:
+        sol = sim.solve()
+    else:
+        sol = sim.solve(starting_solution=solutions[cycle-1].cycles[-1].last_state)
+    solutions[cycle] = sol
+    print("Started at t = ", solutions[cycle].t[0])
+    print("Terminated at t = ", solutions[cycle].t[-1], "s with status", solutions[cycle].termination)
 
-print("Terminated at t = ", sol1.t[-1], "s with status", sol1.termination)
+# full implementation
+experiment_all = batteryProblem(num_cycles, expType=exp)
+sim_all = pybamm.Simulation(
+    model,
+    experiment=experiment_all,
+    parameter_values=parameter_values,
+    solver=solver,
+    var_pts=var_pts,)
+sim_all.build_for_experiment()
+sol_all = sim_all.solve()
 
-last_state = sol1.cycles[-1].last_state
+#%%
+for i, sol in enumerate(sol_all.cycles):
+    print("Started at t = ", sol.t[0])
+    print("Terminated at t = ", sol.t[-1], "s with status", sol.termination)
 
-sol2=sim.solve(starting_solution=last_state)
+#%%
+for cycle in range(num_cycles):
+    sol_step = solutions[cycle]
+    sol = sol_all.cycles[cycle]
+    print("Cycle: ", cycle,"tStart_diff: ", sol_step.t[0]-sol.t[0],"tEnd_diff", sol_step.t[-1] - sol.t[-1])
+    t = np.linspace(sol.t[0], sol.t[-1],1000)
+    
+    err_V = np.max(np.abs(sol_step["Voltage [V]"](t) - sol["Voltage [V]"](t)))
+    print("Error in voltage values: ", err_V)
+    
+    y_step = np.asarray(sol_step.last_state.y).reshape(-1)
+    y_seq = np.asarray(sol.last_state.y).reshape(-1)
 
-print("Terminated at t = ", sol2.t[-1], "s with status", sol2.termination)
+    err_y = np.max(np.abs(y_step - y_seq))
+
+    print(f"Cycle {i}: state error = {err_y}")
